51fa67a8e01bccd04da7ea8baeaa2fb9ff8c39c5
Theirvin1 <icajasi@msuteq.edu.ec>
2026-07-26 22:43:28 -0500

    feat: agregar pruebas unitarias (BackendApplicationTests, AuthServiceTest, LibroServiceTest)

---
+++ DEMO-IRVIN/backend-springboot/src/test/java/com/uteq/backend/BackendApplicationTests.java
@@ -0,0 +1,54 @@
+package com.uteq.backend;
+
+import com.uteq.backend.repository.EditorialRepository;
+import com.uteq.backend.repository.EstadoLibroRepository;
+import com.uteq.backend.repository.EstadoUsuarioRepository;
+import com.uteq.backend.repository.IdiomaRepository;
+import com.uteq.backend.repository.LibroRepository;
+import com.uteq.backend.repository.RolRepository;
+import com.uteq.backend.repository.UsuarioRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=" +
+        "org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration," +
+        "org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration," +
+        "org.springframework.boot.data.jpa.autoconfigure.DataJpaRepositoriesAutoConfiguration," +
+        "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration," +
+        "org.springframework.boot.data.redis.autoconfigure.DataRedisAutoConfiguration," +
+        "org.springframework.boot.data.redis.autoconfigure.DataRedisReactiveAutoConfiguration"
+})
+class BackendApplicationTests {
+
+    @MockitoBean
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @MockitoBean
+    private UsuarioRepository usuarioRepository;
+
+    @MockitoBean
+    private LibroRepository libroRepository;
+
+    @MockitoBean
+    private EditorialRepository editorialRepository;
+
+    @MockitoBean
+    private IdiomaRepository idiomaRepository;
+
+    @MockitoBean
+    private EstadoLibroRepository estadoLibroRepository;
+
+    @MockitoBean
+    private RolRepository rolRepository;
+
+    @MockitoBean
+    private EstadoUsuarioRepository estadoUsuarioRepository;
+
+    @Test
+    void contextLoads() {
+    }
+
+}
